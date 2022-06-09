e382d59bbee3c6f3f597fbb4cb0d49ed9cd21957
shaem03 <muhammedshaheem7@gmail.com>
2022-06-10 04:37:20 +0530

    fixes

--- client/src/region.js
+++ client/src/region.js
@@ -1,6 +1,5 @@
-const base_url = 'http://0.0.0.0:5000'
 export function getRegion() {
-    const url = '/api/region'
+    const url = '/api/regionasdasd'
     return fetch(url)
         .then(data => data.json())
 }
@@ -9,9 +8,9 @@ export function getRegion() {
 export function filterByRegion(abbr, top_n) {
     let url = ""
     if (abbr != null) {
-        url = base_url + '/api/filter?region=' + abbr + "&top=" + top_n
+        url = '/api/filter?region=' + abbr + "&top=" + top_n
     } else {
-        url = base_url + '/api/filter?&top=' + top_n
+        url = '/api/filter?&top=' + top_n
     }
 
     return fetch(url)

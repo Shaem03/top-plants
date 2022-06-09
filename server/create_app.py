38e2046630e543a1e5575f7b1c2c348d0cbb37eb
shaem03 <muhammedshaheem7@gmail.com>
2022-06-10 04:21:44 +0530

    fixes

--- server/create_app.py
+++ server/create_app.py
@@ -1,6 +1,8 @@
 import logging
 
 from flask import Flask
+from flask_cors import CORS
+
 from server.config import config
 from server.view.controllers import mod_api
 from server.view.errors import errors
@@ -10,6 +12,8 @@ from server.view.models import db, ma
 def create_app():
     """For to use dynamic environment"""
     app = Flask(__name__, instance_relative_config=True)
+    cors = CORS(app, resources={r"/api/*": {"origins": "*", "allow_headers": "*", "expose_headers": "*"}})
+
     app.config.from_object(config["api"])
     logging.getLogger('flask_cors').level = logging.DEBUG
     db.init_app(app)

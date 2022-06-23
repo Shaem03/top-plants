5097305a40aec407e1e02c8e01c8b110dd33be3b
shaem03 <muhammedshaheem7@gmail.com>
2022-06-23 23:54:59 +0530

    bug fix

--- server/view/errors.py
+++ server/view/errors.py
@@ -1,8 +1,17 @@
 from flask import Blueprint, Response
 
+from server.view.models import db
+
 errors = Blueprint("errors", __name__)
 
 
 @errors.app_errorhandler(Exception)
 def server_error(error):
     return Response(f"Oops, got an error! {error}", status=500)
+
+
+@errors.teardown_request
+def teardown_request(exception):
+    if exception:
+        db.session.rollback()
+    db.session.remove()
